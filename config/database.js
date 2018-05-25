5bb1da79ad69143ca385a9a0f24dc536778e13ba
k <lookerkj@protonmail.com>
2018-05-24 18:49:38 -0700

    home, login, signup all up

---
+++ config/database.js
@@ -0,0 +1,9 @@
+// var mongoose = require('mongoose');
+// var db = mongoose.createConnection('localhost', 'test');
+
+// module.exports = {db};
+
+module.exports = {
+
+    'url' : 'mongodb://complicationlamp:gradschool1@ds263759.mlab.com:63759/blog-posts-1'
+};
